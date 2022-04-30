7864bdc93182e03892df86e54681028aa4023834
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2022-04-29 22:44:01 -0300

    .env configured

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ import express, { json } from 'express'
 import cors from 'cors'
 import chalk from 'chalk'
 import { MongoClient } from 'mongodb'
-
+import Joi from 'joi'
+import dotenv from 'dotenv'
 
 const app = express()
 app.use(cors())
 app.use(json())
 
+dotenv.config()
 
 let db = null
-const mongoClient = new MongoClient('mongodb://localhost:27017') // process.env.MONGO_URI
+const mongoClient = new MongoClient(process.env.MONGO_URI) // process.env.MONGO_URI
 const promise = mongoClient.connect()
         .then(()=>{
-            db = mongoClient.db('api-bate-papo-uol') // process.env.BANCO
+            db = mongoClient.db(process.env.DATABASE) // process.env.BANCO
             console.log(chalk.blue.bold('Banco de dados conectado com sucesso!'))
         })
         .catch(e => console.log(chalk.red.bold('Problema na conexão com o banco'), e))
+
+const participant = Joi.object({
+    name: Joi.string().required()
+})
+
+const message = Joi.object({
+    from: Joi.string().required(), 
+    to: Joi.string().required(), 
+    text: Joi.string().required(), 
+    type: Joi.string().required()
+})
